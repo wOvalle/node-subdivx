078eed489dc5810b6e526f0ccec063df1ae8aeff
Willy Ovalle <willyovalle16@gmail.com>
2016-08-19 18:05:57 -0400

    Enhancement: Adding download capability

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let request = require('request');
-let $ = require('cheerio');
-let Promise = require('bluebird');
+const request = require('request');
+const fs = require('fs');
+const $ = require('cheerio');
+const Promise = require('bluebird');
+const hat = require('hat');
 
 const SEARCH_SUBDIVX = 'http://subdivx.com/index.php?buscar={q}&accion=5&masdesc=&subtitulos=1&realiza_b=1';
+const SUBS_DIRECTORY = './.subs';
 
 exports.listSubs = function (opts, cb) {
   return new Promise(function (resolve, reject) {
@@ -21,6 +24,7 @@ exports.listSubs = function (opts, cb) {
     exports._request
             .call(this, requestOptions)
             .then(exports._parseHTML)
+            .then(exports._downloadFiles.bind(this, opts))
             .then(resolve)
             .catch(reject);
   });
@@ -52,5 +56,37 @@ exports._parseHTML = function (html) {
             .toArray();
 
     resolve(subs);
+  });
+};
+
+exports._downloadFiles = function (opts, subs) {
+  return new Promise(function (resolve, reject) {
+    if (!opts.download) resolve(subs);
+    if (!fs.existsSync(SUBS_DIRECTORY)) fs.mkdirSync(SUBS_DIRECTORY);
+
+    const promises = subs.map(function (sub) {
+      return new Promise(function (resolve, reject) {
+        const subId = /id=(.*)&/gi.exec(sub.downloadLink)[1] || hat();
+        const r = request(sub.downloadLink);
+
+        r.on('response', function (res) {
+          const fileName = `${SUBS_DIRECTORY}/${subId}.${res.headers['content-type'].split('/')[1]}`;
+          const fileStream = fs.createWriteStream(fileName);
+          res.pipe(fileStream);
+        });
+
+        r.on('error', function () {
+          reject();
+        });
+
+        r.on('close', function () {
+          resolve();
+        });
+      });
+    });
+
+    Promise.all(promises)
+            .then(resolve)
+            .catch(reject);
   });
 };
